bda8069a381b226d66769f09d0ade8eb76a800d3
Andre Rendeiro <afrendeiro@gmail.com>
2016-07-13 16:14:18 +0200

    customized to microtest

--- index.py
+++ index.py
@@ -1,30 +1,15 @@
 #!/usr/bin/env python
+
 import io
 import os
 import re
 import sys
 import json
 import subprocess
-import requests
-import ipaddress
 import hmac
 from hashlib import sha1
 from flask import Flask, request, abort
 
-"""
-Conditionally import ProxyFix from werkzeug if the USE_PROXYFIX environment
-variable is set to true.  If you intend to import this as a module in your own
-code, use os.environ to set the environment variable before importing this as a
-module.
-
-.. code:: python
-
-    os.environ['USE_PROXYFIX'] = 'true'
-    import flask-github-webhook-handler.index as handler
-
-"""
-if os.environ.get('USE_PROXYFIX', None) == 'true':
-    from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.debug = os.environ.get('DEBUG') == 'true'
@@ -34,34 +19,19 @@ app.debug = os.environ.get('DEBUG') == 'true'
 REPOS_JSON_PATH = os.environ['REPOS_JSON_PATH']
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/payload", methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return 'OK'
     elif request.method == 'POST':
-        # Store the IP address of the requester
-        request_ip = ipaddress.ip_address(u'{0}'.format(request.remote_addr))
-
-        # If GHE_ADDRESS is specified, use it as the hook_blocks.
-        if os.environ.get('GHE_ADDRESS', None):
-            hook_blocks = [unicode(os.environ.get('GHE_ADDRESS'))]
-        # Otherwise get the hook address blocks from the API.
-        else:
-            hook_blocks = requests.get('https://api.github.com/meta').json()[
-                'hooks']
-
-        # Check if the POST request is from github.com or GHE
-        for block in hook_blocks:
-            if ipaddress.ip_address(request_ip) in ipaddress.ip_network(block):
-                break  # the remote_addr is within the network range of github.
-        else:
-            abort(403)
 
         if request.headers.get('X-GitHub-Event') == "ping":
             return json.dumps({'msg': 'Hi!'})
         if request.headers.get('X-GitHub-Event') != "push":
+            print("Not a push request")
             return json.dumps({'msg': "wrong event type"})
 
+        print("Loading request")
         repos = json.loads(io.open(REPOS_JSON_PATH, 'r').read())
 
         payload = json.loads(request.data)
@@ -70,6 +40,7 @@ def index():
             'owner': payload['repository']['owner']['name'],
         }
 
+        print("Matching branches")
         # Try to match on branch as configured in repos.json
         match = re.match(r"refs/heads/(?P<branch>.*)", payload['ref'])
         if match:
@@ -81,22 +52,34 @@ def index():
             if not repo:
                 repo = repos.get('{owner}/{name}'.format(**repo_meta), None)
 
+        print("Checking keys")
         if repo and repo.get('path', None):
             # Check if POST request signature is valid
             key = repo.get('key', None)
             if key:
-                signature = request.headers.get('X-Hub-Signature').split(
-                    '=')[1]
+                signature = request.headers.get('X-Hub-Signature').split('=')[1]
                 if type(key) == unicode:
                     key = key.encode()
                 mac = hmac.new(key, msg=request.data, digestmod=sha1)
                 if not compare_digest(mac.hexdigest(), signature):
+                    print("Failed keys")
                     abort(403)
 
+        print("passed all checks")
         if repo.get('action', None):
             for action in repo['action']:
+                print("Doing action %s" % " ".join(action))
+
                 subp = subprocess.Popen(action, cwd=repo.get('path', '.'))
                 subp.wait()
+
+                # Assess if all pass
+                # else:
+                # Send user an email
+                # email = ["commits"]["author"]["email"]
+                # display_name = ["commits"]["author"]["name"]
+                # commit = ["head_commit"]["id"]
+
         return 'OK'
 
 # Check if python version is less than 2.7.7
@@ -128,6 +111,5 @@ if __name__ == "__main__":
         port_number = int(sys.argv[1])
     except:
         port_number = 80
-    if os.environ.get('USE_PROXYFIX', None) == 'true':
-        app.wsgi_app = ProxyFix(app.wsgi_app)
+
     app.run(host='0.0.0.0', port=port_number)
